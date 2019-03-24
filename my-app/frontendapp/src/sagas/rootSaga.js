29d50f6ef6278bd7ffbc69d604a353e48f981da2
iushakov <iushakov@planetasolutions.ru>
2019-03-24 21:17:08 +0100

    rootSaga fixed

--- my-app/frontendapp/src/sagas/rootSaga.js
+++ my-app/frontendapp/src/sagas/rootSaga.js
@@ -4,17 +4,10 @@ import { takeEvery, all} from "redux-saga/effects";
 import watcherAssetSaga from "./api-saga";
 import watcherFilterSaga from "./api-filter-saga";
 
-// export default function* rootSaga() {
-//     yield all([
-//       ...fooSagas,
-//       ...barSagas
-//     ])
-//   }
-
 
 export default function* rootSaga () {
     yield all([
-        watcherAssetSaga(), // saga1 can also yield [ fork(actionOne), fork(actionTwo) ]
+        watcherAssetSaga(),
         watcherFilterSaga()
     ])
 }
